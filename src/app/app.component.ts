bddc4e019c0c144349ab6e917dade4d7e593b602
tony <songjf@yahoo.com>
2024-07-08 01:58:58 -0400

    firsrt commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [HeaderComponent]
 })
-export class AppComponent {
-  title = 'first-angular-app';
-}
+export class AppComponent {}
