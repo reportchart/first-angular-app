420e50092cc541aaff72cfc9df5c05aa74f9df36
tony <songjf@yahoo.com>
2024-07-10 03:29:43 -0400

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
+import { UserComponent } from "./user/user.component";
+
+import { DUMMY_USERS } from "./dummy-users";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [HeaderComponent]
+    imports: [HeaderComponent, UserComponent]
 })
-export class AppComponent {}
+export class AppComponent {
+
+  users=DUMMY_USERS;
+
+
+  onSelectUser_app(id:string)
+  {
+    console.log('Selected user with id'+ id);
+  }
+
+
+
+
+
+
+
+}
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,EventEmitter,Input, Output,output} from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -8,5 +8,35 @@ import { Component } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+
+  @Input({required:true}) id!: string;
+   @Input({required:true}) avatar!: string;
+  @Input({required:true}) name!: string;
+  @Output() selecttony = new EventEmitter<string>();
+
+  //selecttony =output<string>();
+
+
+
+ /*  avatar =input.required<string>();
+  name =input.required<string>(); */
+
+/*   imagePath = computed(()=>{
+
+    return 'assets/users/'+ this.avatar();
+
+  }); */
+
+get imagePath()
+{
+  return 'assets/users/'+ this.avatar;
+}
+
+
+onSelectUser_user() {
+
+  this.selecttony.emit(this.id);
+
+ }
 
 }
